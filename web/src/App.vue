2a707b85eb565ad24daa4c735d262ee04c54aed3
Niklas Lausch <support@niki2k1.de>
2020-09-23 12:58:28 +0200

    Added Header

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,3 +1,10 @@
 <template>
   <router-view/>
 </template>
+<style>
+body {
+  background-image: url('/img/pexels-pixabay-280453.jpg');
+  background-repeat: no-repeat;
+  background-size: 100%;
+}
+</style>
---
+++ web/src/components/LbHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <img class="logo" src="/img/Logo.svg">
+    <div class="form">
+      <h1>Finde Restaurants in deiner Nähe!</h1>
+      <input type="text">
+      <button>Suchen</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LbHeader'
+}
+</script>
+
+<style scoped>
+.header {
+  background-image: url('/img/Header.svg');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  height: 100vh;
+}
+
+.logo {
+  margin: 1rem;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  color: white;
+  justify-content: center;
+  width: 20vw;
+  margin: auto;
+}
+</style>
